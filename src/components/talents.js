c4ea08aeb02b6e2f036af868c1e91c7436c928e7
rollersteaam <rollersteaam@googlemail.com>
2020-03-31 02:37:59 +0100

    Get to hello world

---
+++ src/components/talents.js
@@ -0,0 +1,23 @@
+import React, { Component } from 'react';
+
+
+/**
+ * A list of talents.
+ */
+class Talents extends Component {
+    constructor() {
+        super();
+
+        this.state = {};
+    }
+
+    render() {
+        return (
+            <div>
+                <p>Hello world.</p>
+            </div>
+        )
+    }
+}
+
+export default Talents;

757f611ab4bc537b3abd6c616629d48ea89554a7
rollersteaam <rollersteaam@googlemail.com>
2020-04-04 19:06:28 +0100

    Add notes for future meme on timer impl details

---
+++ src/actions/timerActions.tsx
@@ -0,0 +1,3 @@
+// Make polling timer from TalentTimer view object to repeatedly send dispatch requests?
+// Maybe use sockets? Toggl style system, set it up with backend, should be easy with feather, etc.
+// Hide away request latency from server's authoritative model of the talent's progression and or talent's session
